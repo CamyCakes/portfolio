444336514cc33de8fcffcd72c27471efd15e486d
Cameron Cozza <cameroncozza@gmail.com>
2019-09-21 17:31:48 -0700

    Updates

---
+++ precache-manifest.74c98c079e4a8ba5078cd4a8a857aa8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d834aa3a7a76a3d76ee831f2cb83cc2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9cf53887c9f20c644e42",
+    "url": "/portfolio/static/css/2.73633f82.chunk.css"
+  },
+  {
+    "revision": "2f2029cb50afd38bd88a",
+    "url": "/portfolio/static/css/main.edd76e3d.chunk.css"
+  },
+  {
+    "revision": "9cf53887c9f20c644e42",
+    "url": "/portfolio/static/js/2.5fdd9203.chunk.js"
+  },
+  {
+    "revision": "2f2029cb50afd38bd88a",
+    "url": "/portfolio/static/js/main.3a1a1ca5.chunk.js"
+  },
+  {
+    "revision": "3b51bb4859701ba96661",
+    "url": "/portfolio/static/js/runtime~main.8f3a0260.js"
+  }
+]);
